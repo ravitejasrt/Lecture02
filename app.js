1bf690012a94b2ca5448979a143c12d3afcc43d0
ravitejasrt <raviteja.srt1@gmail.com>
2017-07-14 19:24:41 +0530

    lecture02. modification of appjs

---
+++ app.js
@@ -0,0 +1,30 @@
+var student = {
+  name="",
+  type="student"
+};
+
+document.addEventListener('DOMContentLoaded',contentLoaded);
+
+function contentLoaded(event){
+  document.getElementById('name').addEventListener("keyup",keyUp);
+}
+
+function keyUp(event){
+  calculateNumericOutput();
+}
+
+function calculateNumericOutput(){
+  student.name = document.getElementById('name').value;
+
+  var totalNameValue = 0;
+  for(var i=0; i<student.name.length ; i++){
+    totalNameValue+=student.name.charCodeAt(i);
+  }
+
+  var output = "The total Numeric value of person name is "+totalNameValue;
+  document.getElementById('output').innerTest=output;
+
+
+
+
+}
