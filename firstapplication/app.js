845363ae1ee20904c5a86cadb40cf00c9cc2e7d2
ravitejasrt <raviteja.srt1@gmail.com>
2017-07-15 00:56:41 +0530

    Angularjsapplication1sttest.

--- firstapplication/app.js
+++ firstapplication/app.js
@@ -1,27 +1,27 @@
-..var student = {
-  name="",
-  type="student"
+var student = {
+  name: "",
+  type: "student"
 };
 
-document.addEventListener('DOMContentLoaded',contentLoaded);
+document.addEventListener('DOMContentLoaded', contentLoaded);
 
-function contentLoaded(event){
-  document.getElementById('name').addEventListener("keyup",keyUp);
+function contentLoaded(event) {
+  document.getElementById('name').addEventListener("keyup", keyUp);
 }
 
-function keyUp(event){
+function keyUp(event) {
   calculateNumericOutput();
 }
 
-function calculateNumericOutput(){
+function calculateNumericOutput() {
   student.name = document.getElementById('name').value;
 
   var totalNameValue = 0;
-  for(var i=0; i<student.name.length ; i++){
-    totalNameValue+=student.name.charCodeAt(i);
+  for (var i = 0; i < student.name.length; i++) {
+    totalNameValue += student.name.charCodeAt(i);
   }
 
-  var output = "The total Numeric value of person name is "+totalNameValue;
-  document.getElementById('output').innerTest=output;
-
+  // Insert result into page
+  var output = "Total Numeric value of person's name is " + totalNameValue;
+  document.getElementById('output').innerText = output;
 }
