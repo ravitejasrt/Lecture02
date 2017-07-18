2a0cf5dd627d4afd33f54537b60add6d4f7053fc
ravitejasrt <raviteja.srt1@gmail.com>
2017-07-18 17:56:10 +0530

    prototype inheritence.

---
+++ FilterNgRepeat/filter.js
@@ -0,0 +1,32 @@
+var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("Number array: ", numberArray);
+
+
+var filteredNumberArray1 =numberArray.filter(function(value){
+  return value > 5;
+});
+
+console.log("Filtered number array1: ", filteredNumberArray1);
+
+
+function above5Filter(value) {
+  return value > 5;
+}
+var filteredNumberArray = numberArray.filter(above5Filter);
+console.log("Filtered number array: ", filteredNumberArray);
+
+
+
+
+
+var shoppingList = [
+  "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
+];
+console.log("Shopping List: ", shoppingList);
+
+var searchValue = "Bismol";
+function containsFilter(value) {
+  return value.indexOf(searchValue) !== -1;
+}
+var searchedShoppingList = shoppingList.filter(containsFilter);
+console.log("Searched Shopping List: ", searchedShoppingList);
